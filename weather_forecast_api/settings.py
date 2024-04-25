bacd78881f970445baf6697328e42febf79ab602
Esteban Segura <estebansegura@MacBook-Pro-de-Esteban.local>
2024-04-25 15:50:42 -0600

    feat: Add weather_forecast_api settings and urls config.

--- weather_forecast_api/settings.py
+++ weather_forecast_api/settings.py
@@ -21,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+OPEN_WEATHER_BASE_URL = os.getenv('OPEN_WEATHER_BASE_URL')
+OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
+RESERVAMOS_BASE_URL = os.getenv('RESERVAMOS_BASE_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'weather.apps.WeatherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
